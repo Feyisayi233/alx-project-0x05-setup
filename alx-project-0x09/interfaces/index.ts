196125135b61d747f186bb68391a7f0d6e5c57fd
Feyisayo <feyisayo233@gmail.com>
2025-08-28 13:55:56 +0100

    task 3

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 export interface ReactComponentProps {
   children: ReactNode;
 }
+
 export interface GeneratedImageProps {
   imageUrl: string;
   prompt: string;
@@ -10,5 +11,9 @@ export interface GeneratedImageProps {
   height?: string;
   action: (imagePath: string) => void;
 }
+
+export type RequestProps = {
+  prompt: string;
+};
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
